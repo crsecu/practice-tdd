5ee440dedc6364ef9fa62f3e7ca169f7eb5914ce
Cristina Secu <secu.cristina@yahoo.com>
2025-01-22 13:45:07 -0500

    organize files into pratice folder; install Jest Types and configure project to use it

---
+++ practice/practice.test.js
@@ -0,0 +1,10 @@
+/* 
+TESTS for: 1. A capitalize function that takes a string and returns it with the first character capitalized.
+*/
+
+/* 
+TDD STEPS:
+1. Write a test (bare bone test)
+2. Write code to pass the test (bare bone code, not efficient, not optimized, just write code to pass the test)
+3. If tests passes - refactor code; If test fails - go back to step 2 
+*/
